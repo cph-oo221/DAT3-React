345d3b0b4c0bcf34d0da276f4786100e472eb7a9
cph-oo221 <113044607+cph-oo221@users.noreply.github.com>
2023-11-15 22:15:30 +0100

    add headers + a timer for each req

--- src/file4.jsx
+++ src/file4.jsx
@@ -3,23 +3,33 @@ import React, { useState, useEffect } from "react";
 function JokeComponent() {
   // State to store the joke
   const [joke, setJoke] = useState("");
+  const [lastFetchTime, setLastFetchTime] = useState(null);
 
   // useEffect to fetch the joke
   useEffect(() => {
-    fetch("https://api.chucknorris.io/jokes/random")
-      .then((response) => response.json())
-      .then((data) => setJoke(data.value))
-      .catch((error) => console.error("Error fetching joke:", error));
+    fetchJoke();
   }, []); // Empty dependency array means this runs once on mount
 
-  // Render the joke
-
   function hanleClick() {
-    fetch("https://api.chucknorris.io/jokes/random")
+    const now = new Date().getTime();
+    if (lastFetchTime && now - lastFetchTime < 10000) {
+      console.log("Wait for 10 seconds before fetching a new joke");
+      return;
+    }
+    setLastFetchTime(now);
+    fetchJoke();
+  }
+
+  const fetchJoke = () => {
+    fetch("https://api.chucknorris.io/jokes/random", {
+      headers: {
+        Accept: "application/json",
+      },
+    })
       .then((response) => response.json())
       .then((data) => setJoke(data.value))
       .catch((error) => console.error("Error fetching joke:", error));
-  }
+  };
 
   return (
     <div>
